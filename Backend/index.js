01a3bcfee059c7e3628fcac9d7021dd64ff26dd9
Hashem Hasan <41197608+Hashem-Hasan@users.noreply.github.com>
2024-07-05 14:57:12 +0300

    API FIXES

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // replace with your frontend's URL
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // replace with your frontend's URL
   credentials: true,
 }));
 app.use(bodyParser.json());
