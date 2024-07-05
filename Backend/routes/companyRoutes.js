7d0d88fe336474e5d2bc4187b50fb0600e60a006
Hashem Hasan <41197608+Hashem-Hasan@users.noreply.github.com>
2024-07-05 15:34:14 +0300

    Final Fixes

--- Backend/routes/companyRoutes.js
+++ Backend/routes/companyRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../models/User');
+const User = require('../models/user');
 const BpmnModel = require('../models/BpmnModel');
 const authenticateToken = require('../middleware/authenticateToken');
 const authenticateEmployeeToken = require('../middleware/authenticateEmployeeToken');
